4a1fb8c3d674e668b73cb5d9074095cc231690e3
1537216636-spec <1537216636@qq.com>
2026-06-30 20:55:33 +0800

    添加所有项目文件

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -12,7 +12,7 @@ import Photos
 
 // Source: https://pub.dev/packages/receive_sharing_intent
 class ShareViewController: SLComposeServiceViewController {
-    let hostAppBundleIdentifier = "com.weikang.yourapp"
+    let hostAppBundleIdentifier = "com.weikang.appname"
     let sharedKey = "ShareKey"
     var sharedMedia: [SharedMediaFile] = []
     var sharedText: [String] = []
